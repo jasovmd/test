8d3396ae5bf35776b3b6161ff5e0fdb193e74a52
jasovmd <jasonkuan0504@gmail.com>
2024-03-12 12:49:09 +0800

    Update Bob.py

--- Bob.py
+++ Bob.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
-intents.all()
+intents.members=True
 
 bot = commands.Bot(command_prefix='[', intents=intents)
 
@@ -13,9 +13,13 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     print(f'{member} join!')
+    channel = bot.get_channel(1216933471084351578)
+    await channel.send(f'{member} join!')
 
 @bot.event
 async def on_member_remove(member):
     print(f'{member} leave!')
+    channel = bot.get_channel(1216933648612200488)
+    await channel.send(f'{member} leave!')
 
-bot.run('MTIxNjU5Nzk1NDUxMzg2NjgzMw.G0Sub9.VVCQAFF3f81E44_coYCDh0DwhLKIP-QiWclT_A')
+bot.run('MTIxNjU5Nzk1NDUxMzg2NjgzMw.GR6T7i.zDo3Rqjshkj2EkpK7abvrUVUCnzSu4HbZNQXH4')
